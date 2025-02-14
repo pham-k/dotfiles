9cdcb81221f04295e025b59bc76e19bf0116193a
pham-k <pham.k@protonmail.com>
2025-02-14 04:43:39 -0500

    Add go path on linux

--- zsh/.zshrc.d/environment.sh
+++ zsh/.zshrc.d/environment.sh
@@ -2,6 +2,7 @@
 typeset -U PATH path
 
 export GOPATH="$HOME/go"
+export PATH="/usr/local/go/bin:$PATH"
 export PATH="$GOPATH/bin:$PATH"
 export PATH="$HOME/.local/bin:$PATH"
 export XDG_CONFIG_HOME="$HOME/.config"
