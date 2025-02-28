436d38c29d0a1834bc5d3d1cb5bb0ee53d361d0e
Pham Khuong <pham.k@protonmail.com>
2025-02-28 16:01:28 +0700

    Update config

--- zsh/.zshrc.d/environment.sh
+++ zsh/.zshrc.d/environment.sh
@@ -7,3 +7,9 @@ export PATH="$GOPATH/bin:$PATH"
 export PATH="$HOME/.local/bin:$PATH"
 export XDG_CONFIG_HOME="$HOME/.config"
 export HOMEBREW_NO_AUTO_UPDATE=1
+
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+
+eval "$(pyenv init - zsh)"
+eval "$(pyenv virtualenv-init -)"
